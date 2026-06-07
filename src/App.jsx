c8f309b09486d8be27bb03a530a1c35e9edcddf2
Your Name <you@example.com>
2026-06-08 03:09:05 +0900

    추가 수정 사항 반영

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import SimpleHeader from './components/SimpleHeader'
+import FloatingButton from './components/FloatingButton'
 import Home from './pages/Home'
+import CustomExchange from './pages/CustomExchange'
 import AgentKIntro from './pages/AgentKIntro'
 import Notice from './pages/Notice'
 import FAQ from './pages/FAQ'
 import ExchangeGuide from './pages/ExchangeGuide'
 import PrivacyPolicy from './pages/PrivacyPolicy'
+import ReservationHistory from './pages/ReservationHistory'
 
 function App() {
   return (
@@ -14,12 +16,15 @@ function App() {
       <SimpleHeader />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/custom-exchange" element={<CustomExchange />} />
         <Route path="/shop01" element={<AgentKIntro />} />
         <Route path="/notice" element={<Notice />} />
         <Route path="/faq" element={<FAQ />} />
         <Route path="/info01" element={<ExchangeGuide />} />
         <Route path="/policy" element={<PrivacyPolicy />} />
+        <Route path="/reservation-history" element={<ReservationHistory />} />
       </Routes>
+      <FloatingButton />
     </Router>
   )
 }
